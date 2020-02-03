77a59bf6900f7ae5e815518708b72ad4be1fc038
hcy <775656764@qq.com>
2020-02-03 17:27:54 +0800

    111

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div class="app-container">
+       <el-button class="filter-item" style="margin-left: 10px;" type="primary" icon="el-icon-edit" @click="handleCreate">
+        Add
+      </el-button>
+    <el-button  :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
+        Export
+    </el-button>
+    <br/><br/>
     <el-checkbox-group v-model="choiceFields">
       <el-checkbox :label="index" v-for="(item,index) in defultFields" checked>{{item.alias}}</el-checkbox>
     </el-checkbox-group>
@@ -32,9 +39,38 @@
         </template>
       </el-table-column>
     </el-table>
+    <br />
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="currentPage"
+       :page-sizes="[6, 10, 100, 200,300,400]"
+      :page-size="pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="totalNum">
+    </el-pagination>
 
-    <el-dialog title="预览" :visible.sync="dialogPreviewVisible">
+    <el-dialog title="Preview" :visible.sync="dialogPreviewVisible">
       <img width="98%;" style=" margin-left:1%; " :src="previewImageUrl" />
+    </el-dialog>
+
+    <el-dialog title="Add" :visible.sync="dialogAddVisible">
+
+      <el-form ref="form" :model="addform" label-width="80px">
+
+  <el-form-item :label="item.alias" v-for="(item,index) in  insertFields" >
+    <el-input v-model="addform.name" v-if="item.webFieldType == 'String'"></el-input>
+    <el-input v-model="addform.name" v-if="item.webFieldType == 'Number'"></el-input>
+  </el-form-item>
+
+
+  <el-form-item>
+    <el-button type="primary" @click="onSubmit">Confirm</el-button>
+    <el-button>Cancel</el-button>
+  </el-form-item>
+</el-form>
+
+
     </el-dialog>
   </div>
 </template>
@@ -45,10 +81,18 @@ import { getList } from "@/api/table";
 export default {
   data() {
     return {
+      addform:[],
+      dialogAddVisible:false,
+      totalNum:100,
+      pageSize:6,
+      currentPage:1,
+      downloadLoading: false,
       table: this.$route.name,
       tableData: [],
       choiceFields: [],
       fields: {},
+      fieldsNameArr:[],
+      fieldsAliasNameArr:[],
       defultFields: {},
       updateFields:{},
       insertFields:{},
@@ -58,15 +102,61 @@ export default {
     };
   },
   mounted() {
-    var that = this;
+      this.initData();
+  },
+  methods: {
+      handleCreate(){
+         
+      },
+     handleSizeChange(val) {
+         this.pageSize = val;
+        this.initData();
+      },
+    handleCurrentChange(val) {
+         this.currentPage = val;
+         this.initData();
+      },
+    handleClick(row) {
+      console.log(row);
+    },
+    previewImage(data) {
+      this.previewImageUrl = data;
+      this.dialogPreviewVisible = true;
+    },
+    handleDownload(){
+      this.downloadLoading = true
+      var that = this;
+      import('@/vendor/Export2Excel').then(excel => {
+        const tHeader = that.fieldsAliasNameArr
+        const filterVal =that.fieldsNameArr
+        const data = that.formatJson(filterVal)
+        excel.export_json_to_excel({
+          header: tHeader,
+          data,
+          filename: 'table-'+that.table
+        })
+        this.downloadLoading = false
+      })
+    },
+     formatJson(filterVal) {
+      return this.tableData.map(v => filterVal.map(j => {
+        if(v[j].length > 200){
+          return "数据太长不显示";
+        }else{
+          return v[j];
+        }
+      }))
+    },
+    initData(){
+         var that = this;
     var data = {
       u: "gtable",
       table: that.table,
-      pagenum: 3,
-      pagesize: 8
-      // like: {
-      //   id: "452760"
-      // }
+      pagenum: that.currentPage,
+      pagesize: that.pageSize
+    //   like: {
+    //    // id: "452760"
+    //   }
     };
     getList(data)
       .then(response => {
@@ -75,6 +165,7 @@ export default {
         that.tableData = data.data;
         that.fields = data.fields;
         that.defultFields = data.fields;
+           that.totalNum = data.total
         var updateFields = {};
         var insertFields = {};
         for(var key in data.fields){
@@ -87,30 +178,28 @@ export default {
         }
        that.insertFields = insertFields;
        that.updateFields = updateFields;
-
+ 
       })
       .catch(error => {
         alert("加载数据出错")
       });
-  },
-  methods: {
-    handleClick(row) {
-      console.log(row);
-    },
-    previewImage(data) {
-      this.previewImageUrl = data;
-      this.dialogPreviewVisible = true;
     }
   },
   watch: {
     choiceFields(valArr) {
       var fields = {};
+      var fieldsNameArr = []
+      var fieldsAliasNameArr = []
       for (var key in this.defultFields) {
         if (valArr.indexOf(key) >= 0) {
           fields[key] = this.defultFields[key];
+          fieldsNameArr.push(key)
+          fieldsAliasNameArr.push(this.defultFields[key]["alias"])
         }
       }
       this.fields = fields;
+      this.fieldsNameArr = fieldsNameArr;
+      this.fieldsAliasNameArr = fieldsAliasNameArr;
       // this.fields = this.defultFields.filter(i => valArr.indexOf(i) >= 0)
     }
   }
